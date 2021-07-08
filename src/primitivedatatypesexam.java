80e66d0ff25a50eb7b38b5f81cc4846ca030e1eb
ParkJooahn <ParkJooahn@192.168.0.228>
2021-07-08 15:14:34 +0900

    Initial Commut

---
+++ src/primitivedatatypesexam.java
@@ -0,0 +1,12 @@
+ public class primitivedatatypesexam {
+	 public static void main (String[] args) {
+		 boolean isFun = false;
+		 	System.out.println(isFun);
+		 int x = 121;
+		 	System.out.println(x);
+		 long bing = 12123123123l;
+			System.out.println(bing);
+		float f = 123.12345f;
+			System.out.println(f);
+	 }
+ }
